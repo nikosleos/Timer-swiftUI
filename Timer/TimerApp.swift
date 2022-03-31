08abe523ae5157aea20ce9488d8fe933ab012140
Nikos Leonov <n_leonov@protonmail.com>
2022-04-01 02:19:30 +0300

    Initial Commit

---
+++ Timer/TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerApp.swift
+//  Timer
+//
+//  Created by macbook on 01/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
